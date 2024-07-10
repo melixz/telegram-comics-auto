27a44b4c808a1a7a74c44bc93d30b1023e6891be
Vlad <dr.melix@gmail.com>
2024-07-10 14:59:13 +0300

    Изменил функцию get_comics.py для загрузки случайных комиксов

--- get_comics.py
+++ get_comics.py
@@ -2,10 +2,11 @@ import os
 import requests
 import argparse
 from common import download_image, get_file_extension_from_url
+import random
 
 
-def fetch_xkcd_comic(comic_num):
-    url = f"https://xkcd.com/{comic_num}/info.0.json"
+def fetch_xkcd_comic(random_number):
+    url = f"https://xkcd.com/{random_number}/info.0.json"
     response = requests.get(url)
     response.raise_for_status()
     comic_data = response.json()
@@ -21,11 +22,12 @@ def save_xkcd_comic(image_url, folder_name='comics'):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Загрузить комикс XKCD по номеру')
-    parser.add_argument('--comic_num', type=int, default=353, help='Номер комикса XKCD')
+    parser = argparse.ArgumentParser(description='Загрузить случайный комикс XKCD')
+    parser.add_argument('--max_num', type=int, default=3539, help='Максимальный номер комикса XKCD')
     args = parser.parse_args()
 
-    image_url = fetch_xkcd_comic(args.comic_num)
+    random_number = random.randint(1, args.max_num)
+    image_url = fetch_xkcd_comic(random_number)
     save_xkcd_comic(image_url)
 
 
