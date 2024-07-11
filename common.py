c6e1fb5b1b1b77a70f17f2de5667f08c3d4c2128
Vlad <dr.melix@gmail.com>
2024-07-11 21:16:51 +0300

    Вынес создание директории в main() для улучшения атомарности функции download_image()

--- common.py
+++ common.py
@@ -4,7 +4,6 @@ from urllib.parse import urlsplit, unquote
 
 
 def download_image(url, save_path):
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
     with open(save_path, 'wb') as file:
@@ -17,3 +16,16 @@ def get_file_extension_from_url(url):
     filename = os.path.basename(unquote(path))
     _, extension = os.path.splitext(filename)
     return extension
+
+
+def main():
+    url = "http://example.com/image.jpg"
+    save_path = "downloads/image.jpg"
+
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+    download_image(url, save_path)
+
+
+if __name__ == "__main__":
+    main()
